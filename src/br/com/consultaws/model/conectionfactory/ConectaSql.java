addf75bafc95bc949ce1c45df66d1bbb3e1ec446
marcelo <marcelo@DESKTOP-5KGEGLR>
2019-01-19 12:08:50 -0300

    commit

--- src/br/com/consultaws/model/conectionfactory/ConectaSql.java
+++ src/br/com/consultaws/model/conectionfactory/ConectaSql.java
@@ -6,6 +6,7 @@ import java.sql.SQLException;
 
 public class ConectaSql {
 
+	
 	private static final String URL = "jdbc:postgresql://localhost/marcar_consulta";
 	private static final String USER = "postgres";
 	private static final String PASSWORD = "1234";
